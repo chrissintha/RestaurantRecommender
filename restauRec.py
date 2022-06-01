bab2af64fed381cd0df2542998b6fd2547764050
Chrissintha <99208992+chrissintha@users.noreply.github.com>
2022-06-01 15:35:16 +0200

    Update restauRec.py

--- restauRec.py
+++ restauRec.py
@@ -1,3 +1,11 @@
 import streamlit as st
 import pandas as pd
-st.write("Hello")
+chefmozaccepts=pd.read_csv('chefmozaccepts.csv')
+chefmozcuisine=pd.read_csv('chefmozcuisine.csv')
+chefmozhours4=pd.read_csv('chefmozhours4.csv')
+chefmozparking=pd.read_csv('chefmozparking.csv')
+geoplaces2=pd.read_csv('geoplaces2.csv')
+rating_final=pd.read_csv('rating_final.csv')
+usercuisine=pd.read_csv('usercuisine.csv')
+userpayment=pd.read_csv('userpayment.csv')
+userprofile=pd.read_csv('userprofile.csv')
