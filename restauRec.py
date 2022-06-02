d29fb8ae0cdb11dae3416c95a146a92a6a4999ea
Chrissintha <99208992+chrissintha@users.noreply.github.com>
2022-06-02 09:31:28 +0200

    Update restauRec.py

--- restauRec.py
+++ restauRec.py
@@ -85,10 +85,18 @@ most_popular1 = city_based_recommender(matrix1.copy(),city)
 mostPopular1 = most_popular1.filter(['name','city','Rcuisine'])
 st.table(mostPopular1)
 
-# col1, col2, col3 = st.columns(3)
-col1, col2,  col3 = st.beta_columns([6,1])
+col1, col2, col3 = st.columns(3)
 
 with col1:
+ def _max_width_(prcnt_width:int = 75):
+    max_width_str = f"max-width: {prcnt_width}%;"
+    st.markdown(f""" 
+                <style> 
+                .reportview-container .main .block-container{{{max_width_str}}}
+                </style>    
+                """, 
+                unsafe_allow_html=True,
+    )
     st.subheader("Rating Based : ")
     #select Rating for city
     rating = st.selectbox(
